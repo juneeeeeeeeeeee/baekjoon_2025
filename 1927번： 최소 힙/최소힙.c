d9ef4cae3bcdb627054e1e926d9ef422c8102db1
Jun <june029882@yonsei.ac.kr>
2025-03-27 01:53:01 +0900

    minheap 공부

---
+++ 1927번： 최소 힙/최소힙.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct _minheap{
+    unsigned int arr[100001];
+    int size;
+} minheap;
+void push(minheap* hp, unsigned int n)
+{
+    hp->size++;
+    int findingpos = hp->size;
+    while(findingpos >= 1 && hp->arr[findingpos >> 1] > n)
+    {
+        hp->arr[findingpos] = hp->arr[findingpos >> 1];
+        findingpos = findingpos >> 1;
+    }
+    hp->arr[findingpos] = n;
+}
+unsigned int pop(minheap *hp)
+{
+    if(!hp->size)
+        return 0;
+    unsigned int r = hp->arr[1];
+    unsigned int last = hp->arr[hp->size];
+    hp->size--;
+    int parent = 1;
+    int child = 2;
+    while(child <= hp->size)
+    {
+        if(child + 1 <= hp->size && hp->arr[child] > hp->arr[child+1])
+            child++;
+        if(hp->arr[child] > last)
+            break;
+        hp->arr[parent] = hp->arr[child];
+        parent = child;
+        child = child << 1;
+    }
+    hp->arr[parent] = last;
+    return r;
+}
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    minheap* hp = malloc(sizeof(minheap));
+    hp->size = 0;
+    while(N--)
+    {
+        unsigned int a;
+        scanf("%u", &a);
+        if(!a)
+            printf("%u\n", pop(hp));
+        else
+            push(hp, a);
+    }
+    free(hp);
+    return 0;
+}
